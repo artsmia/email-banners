a6aa9cbe329cec6fbb8bcf3c5a1589d92df447da
Kjell <kjell@leanside.com>
2013-09-05 12:55:10 -0500

    New banner for Audacious Eye

---
+++ activate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+image=$1
+
+# Move it to the originals directory
+if [ ! -f originals/$image ]; then
+  mv $image originals/$image
+fi
+
+# (1) Resize, compress, save to $image; (2) copy to MIA_exhibition-signature.jpg; (3) rsync
+gm convert -resize 350x -compress JPEG -quality 90 originals/$image $image
+cp $image MIA-exhibition-signature.jpg
+rsync -av * /Volumes/d\$/WebSites/artsmia-redesign/images/email-banners
